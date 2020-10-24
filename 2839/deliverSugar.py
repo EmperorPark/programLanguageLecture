832ba0b87f5f3f2005a9fdab0a17528afd083b3e
Emperor <woghkd0719@gmail.com>
2020-10-24 10:50:47 +0900

    2839 풀이 완료

---
+++ 2839/deliverSugar.py
@@ -0,0 +1,52 @@
+# BACKJOON Online Judge #2839
+
+import sys
+
+def func():
+    for i in range(0, 5001, 3):
+        # if i % 3 != 0:
+        #    continue
+        for j in range(0,5001, 5):
+            # if j % 5 != 0:
+            #    continue
+            if i + j == N:
+                x = j
+                y = i
+                return [x, y]
+            if i > N and j > N:
+                return -1
+
+
+
+
+#f = sys.stdin
+f = open('Week8/2839/data.txt', 'r')
+N = int(f.readline())
+
+r = func()
+
+if r == -1:
+    print(r)
+else:
+    print(r[0]//5 + r[1]//3)
+
+
+"""
+import sys
+f = sys.stdin
+#f = open('data.txt', 'r')
+
+N = int(f.readline().rstrip())
+temp = N
+result = -1
+
+for i in range(0, N, 3):
+    temp = N - i
+    if temp % 5 == 0:
+        result = (temp//5 + i//3)
+    break
+    if result == -1:
+        if N % 3 == 0:
+            result = N // 3
+print (result)
+"""
